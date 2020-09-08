fcd36d405cdf3c65f1af90f4f7ba4367c0187e41
Karthikeyan <clickforkarthik@gmail.com>
2020-09-08 21:49:47 +0200

    Add files via upload

---
+++ first-springboot-microservice/src/main/java/com/example/firstspringbootmicroservice/FirstSpringbootMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.example.firstspringbootmicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstSpringbootMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstSpringbootMicroserviceApplication.class, args);
+	}
+
+}
---
+++ first-springboot-microservice/src/test/java/com/example/firstspringbootmicroservice/FirstSpringbootMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.firstspringbootmicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstSpringbootMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

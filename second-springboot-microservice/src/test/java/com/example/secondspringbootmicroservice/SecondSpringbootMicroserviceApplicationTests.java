bcf1094bf2fd30b442b0d6d394e8773a5a16f778
Karthikeyan <clickforkarthik@gmail.com>
2020-09-08 21:50:24 +0200

    Add files via upload

---
+++ second-springboot-microservice/src/test/java/com/example/secondspringbootmicroservice/SecondSpringbootMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.secondspringbootmicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecondSpringbootMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
